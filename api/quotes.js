18da98bbfec4fc01563d9bfa73b6ddd38d5346ee
Hardik Jain <74176758+hardikjain826@users.noreply.github.com>
2025-01-04 02:37:39 +0530

    Set [false] strictQuery for Mongoose to suppress warning

--- api/quotes.js
+++ api/quotes.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Quote = require('../models/Quote');
 
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', false);
 const connectToDatabase = async () => {
   if (!mongoose.connection.readyState) {
     await mongoose.connect(process.env.MONGO_URI, {
